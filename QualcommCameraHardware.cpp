00aefc6e4ee20e011bcf2704bd80f3fc3c26111f
Tomasz Rostanski <rostan@podkowa.net>
2013-01-01 20:55:56 +0100

    Set KEY_SUPPORTED_VIDEO_SIZES parameter

--- QualcommCameraHardware.cpp
+++ QualcommCameraHardware.cpp
@@ -1478,6 +1478,8 @@ void QualcommCameraHardware::initDefaultParameters()
 
     mParameters.set(CameraParameters::KEY_SUPPORTED_PREVIEW_SIZES,
                     preview_size_values.string());
+    mParameters.set(CameraParameters::KEY_SUPPORTED_VIDEO_SIZES,
+                    preview_size_values.string());
 
     mParameters.set(CameraParameters::KEY_SUPPORTED_PICTURE_SIZES,
                     picture_size_values.string());
